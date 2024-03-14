a3006bef14445e3ecbf4e1f2ea75f3e027a89bf6
Josh <73722363+Sleepy-Git-User@users.noreply.github.com>
2024-03-14 13:10:21 +0000

    Added testing for Hash Function
    Tested using mocha

--- server/test/test.js
+++ server/test/test.js
@@ -1,7 +1,15 @@
+const hash = require('../ulti/hash.js');
 var expect = require('expect.js');
 
 describe('Test', function() {
     it('should return 1', function() {
         expect(1).to.equal(1);
+    });
+});
+
+describe('makeSalt', function(){
+    it('should return 1', function() {
+        const CreateSalt = hash.makeSalt();
+       expect(CreateSalt).to.have.length(128);
     });
 });
