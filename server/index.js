e0bbf22454762309e110c763d05bfde1b1833e55
Wilsonator123 <106590637+Wilsonator123@users.noreply.github.com>
2024-03-25 18:48:30 +0000

    Added Cookie Functionality - [x] Added Cookie Creator - [x] Added Cookie Validator

--- server/index.js
+++ server/index.js
@@ -1,13 +1,16 @@
 const express = require('express')
+const cookieParser = require('cookie-parser')
 const app = express()
 const port = 8000
 const db = require('./database/index.js')
+
+app.use(express.json())
+app.use(cookieParser())
 
 app.get('/', async (req, res) => {
     const result = await db.query('SELECT NOW()').catch(e => console.error(e.stack))
     res.json(result?.rows)
 })
-
 
 app.listen(port, () => {
     console.log(`Example app listening on port ${port}`)
--- server/ulti/cookie.js
+++ server/ulti/cookie.js
@@ -1,1 +1,27 @@
-// TODO Create cookies for authenticating users
+const { createAndSignJWT, verifyJWT } = require('./auth');
+
+
+async function setCookie(res, userID) {
+    const jwt = await createAndSignJWT(userID)
+    res.cookie('id', jwt, {httpOnly: true, secure: true, sameSite: 'none', })
+}
+
+async function validateCookie(req, res) {
+    const jwt = req.cookies.id
+    const result = await verifyJWT(jwt)
+    if(result === "Invalid JWT") {
+        clearCookie(res)
+    } else {
+        return result
+    }
+}
+
+function clearCookie(res) {
+    res.cookie('id', '', { expires: new Date(0), httpOnly: true, secure: true, sameSite: 'none' })
+    return "Cookie cleared"
+}
+
+module.exports = {
+    setCookie,
+    validateCookie,
+}
