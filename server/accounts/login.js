c733252a1a36415f17a16e42bb04c7f781dc9be4
Sleepy <joshfranks910@gmail.com>
2024-03-26 00:45:04 +0000

    Alot of stuff
    Account creation function added with some validation, Login function added with some validation and standard replies, Query function re made to be promise based, UUID function added with collision checking

---
+++ server/accounts/login.js
@@ -0,0 +1,35 @@
+const db = require('../database/index.js');
+const hash = require('../ulti/hash.js')
+var validator = require("email-validator");
+
+async function login(email,password){
+   
+     //Checks if the email is valid using regex (library)
+     if (validator.validate(email) == false){
+        return "Email is not valid"
+    }
+
+    //Checks to see if the email has been taken.
+    if ((await db.query("isEmail", [email]))[0].count == 0){
+        return "Email or Password incorrect"
+    } 
+
+    let userID = (await db.query("getUserID",[email]))[0].userid
+
+    //Gets the users salt and hashed password based on the UserID their email gave
+    let userSalt = (await db.query("getUserSalt",[userID]))[0].salt
+    let userHashedPassword = (await db.query("getUserPassword",[userID]))[0].password
+
+    //Hashes the password Attempt using the stored salt
+    let passwordAttempt = hash.saltNhash(password,userSalt);
+
+    //Compares the hash password Attempt with the Stored hashed password.
+    if (userHashedPassword != passwordAttempt){
+        return "Inccorect Username and Password"
+    }
+
+    return "Correct Username and Password"
+
+}
+
+//login("test@tessst.om","nuts123").then((result) => console.log(result)).catch((err) => console.error(err));
