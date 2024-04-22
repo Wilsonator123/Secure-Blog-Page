1fd96004feb3e08f995f81a69ddd71efa40d6ac3
Wilsonator123 <106590637+Wilsonator123@users.noreply.github.com>
2024-04-22 15:29:50 +0100

    Middleware changes
    - Added `helmet js` library which alters http header options to be more secure
    - Added API limiter which allow 10 requests a second with a 5 minutes block period

--- server/index.js
+++ server/index.js
@@ -2,14 +2,37 @@
 const path = require("path");
 const express = require('express');
 const cors = require('cors');
-
 const app = express();
 const port = 8000;
 const db = require('./database/index.js');
+const helmet = require('helmet');
+const { RateLimiterMemory } = require('rate-limiter-flexible');
 
 app.use(express.json()); // for parsing application/json
 app.use(express.urlencoded({ extended: true }));
 app.use(cors());
+app.use(helmet());
+
+const opts = {
+	points: 10, // 100 requests
+	duration: 1, // Per second
+	blockDuration: 5*60
+};
+
+const rateLimiter = new RateLimiterMemory(opts);
+
+const rateLimiterMiddleware = (req, res, next) => {
+	rateLimiter.consume(req.ip)
+		.then(() => {
+			next();
+		})
+		.catch(() => {
+			res.status(429).send('Too Many Requests');
+		});
+};
+
+app.use(rateLimiterMiddleware);
+
 
 // Server Middleware Loging to Console
 app.use((req, res, next) => {
@@ -19,14 +42,12 @@ app.use((req, res, next) => {
 	console.log("Request made to", req.path, "took", `${time}ms`);
 });
 
-
 // Routes
 app.use("/apiTest", require('./routes/apiTest.js')); //
 app.use("/login", require('./routes/login.js'));
 
 app.get('/', async (req, res) => {
-    const result = await db.query('SELECT NOW()').catch(e => console.error(e.stack))
-    res.json(result?.rows)
+    res.json({ message: 'Hello World!' })
 })
 
 
