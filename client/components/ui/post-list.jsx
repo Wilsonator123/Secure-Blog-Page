43d8ed6c310f4e145575394d50f4e8badea6b8ec
Wilsonator123 <106590637+Wilsonator123@users.noreply.github.com>
2024-04-29 13:35:36 +0100

    This works for me

--- client/components/ui/post-list.jsx
+++ client/components/ui/post-list.jsx
@@ -1,25 +1,32 @@
-import React from "react";
-import usePosts from "@/hooks/usePosts";
+import React, {useEffect, useState} from "react";
+import { usePosts } from "@/hooks/usePosts";
 
 
 export default function PostList() {
-    const { posts, loading, error} = usePosts();
-    console.log(posts);
-    
+    const [posts, setPosts] = useState([])
+    const [loading, setLoading] = useState(true);
 
-    if (loading) {
-        return <div className="text-text text-2xl">Loading...</div>;
-    }
-
-    if (error) {
-        return <div className="text-text text-2xl">Error loading posts.</div>;
-    }
+    useEffect(() => {
+        usePosts()
+            .then((response) => {
+                setPosts(response);
+                setLoading(false);
+            });
+    }, []);
 
     return (
         <div>
-            {posts.map((post) => (
-                <Post key={post.id} post={post}/>
-            ))}
+            {loading ? <p>Loading...</p> :
+                <>
+                    {posts.map((post) => (
+                        <div key={post.id} className="flex flex-col my-4 p-4 bg-primary border-border border-secondary">
+                            <p className="text-text text-2xl">{post.title}</p>
+                            <p className="text-text text-lg">{post.content}</p>
+                            <p className="text-text text-lg">Votes: test</p>
+                        </div>
+                    ))}
+                </>
+            }
         </div>
     );
 }
--- client/hooks/usePosts.js
+++ client/hooks/usePosts.js
@@ -1,28 +1,13 @@
-import { useState, useEffect } from 'react';
+'use server'
 import axios from 'axios';
 
-const usePosts = () => {
-    const [posts, setPosts] = useState([]);
-    const [loading, setLoading] = useState(true);
-    const [error, setError] = useState(null);
-
-    useEffect(() => {
-        const fetchPosts = async () => {
-            try {
-                const response = await axios.post('http://localhost:8000/posts/getPosts');
-                setPosts(response.data);
-                setLoading(false);
-            } catch (error) {
-                console.error('Error fetching posts:', error);
-                setError(error);
-                setLoading(false);
-            }
-        };
-
-        fetchPosts();
-    }, []);
-
-    return { posts, loading, error };
-};
-
-export default usePosts;
+export const usePosts = async () => {
+    'use server'
+    try {
+        const response = await axios.post('http://localhost:8000/posts/getPosts')
+        return response.data.data
+    } catch (error) {
+        console.error(error)
+        return []
+    }
+}
