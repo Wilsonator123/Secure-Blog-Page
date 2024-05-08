1a5d58fd1b9b6f3739abd81b332f563c485848b1
Alex Taft <126822918+thealextaft@users.noreply.github.com>
2024-05-08 01:16:20 +0100

    Demo of the potential settings This is all in one file as asked george not sure on how i will function the button across all types of submit but i will figure it out

--- client/components/ui/settings-page.jsx
+++ client/components/ui/settings-page.jsx
@@ -1,6 +1,6 @@
 'use client'
 
-import React, { useState } from 'react';
+import React, { useState, useRef } from 'react';
 import { useRouter } from 'next/navigation';
 import { Card, CardHeader, CardTitle, CardContent, CardFooter } from "@/components/ui/card";
 import UserPFP from '@/components/ui/user-pfp';
@@ -12,11 +12,23 @@ import Placeholder from '@/assets/placeholder.svg';
 import Close from '@/assets/close.svg';
 import Logout from '@/assets/logout.svg';
 import Return from '@/assets/return.svg';
+import ShowPassword from '@/assets/showPassword.svg'
+import HidePassword from '@/assets/hidePassword.svg'
+import { Input } from "@/components/ui/input";
 const API_URL = 'http://127.0.0.1:8000/';
 
 export default function SettingsPage({ toggle }) {
   const [activeSetting, setActiveSetting] = useState('');
   const router = useRouter();
+
+  const passwordBox = useRef();
+  const emailBox = useRef();
+  
+  //form data states
+  const [showPassword, setShowPassword] = useState("password");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [error, setError] = useState("");
 
   const handleSignOut = async () => {
     try {
@@ -43,9 +55,42 @@ export default function SettingsPage({ toggle }) {
   const renderSettingForm = () => {
     switch(activeSetting) {
       case 'email':
-        return <div className='text-text'>Form to change email</div>;
+        return (
+          <section>
+            <h2 className="text-text text-2xl font-bold text-center mb-6">Change Email</h2>
+
+
+            <div className="relative flex flex-col w-full justify-center items-center">
+                <p className='text-text absolute -top-2 left-1'>Password</p>
+                <Input id="login-password" type={`${showPassword}`} className="my-4 h-14 bg-black border-secondary text-text pl-12 focus:border-accent"
+                required value={password} autoComplete="current-password" onChange={(e) => setPassword(e.target.value)} ref={passwordBox}/>
+
+                <div className="text-text absolute left-2 z-10">
+                  <PasswordPin  fill={'#fff'}/>
+                </div> 
+
+                <div onClick={() => {setShowPassword(prev => prev === 'text' ? 'password' : 'text') 
+                passwordBox.current.focus();
+
+                }} 
+                className="text-text absolute right-2 z-10">
+                  {showPassword === 'password' ? <HidePassword width={30} height={30} fill={'#fff'}/> : <ShowPassword width={30} height={30} fill={'#fff'}/>}
+              </div>
+            </div>
+
+            <div className="relative flex flex-col w-full justify-center items-center pb-6">
+              <p className='text-text absolute -top-2 left-1'>New E-mail</p>
+              <Input id="login-email" type="email" className="my-4 h-14 bg-black border-secondary text-textpl-12 focus:border-accent"
+              required value={email} autoComplete="email" ref={emailBox} />
+
+              <div className="text-text absolute left-2 z-10">
+                <Mail width={30} height={30} fill={'#fff'}/>
+              </div>
+            </div>
+          </section>
+        );
       case 'password':
-        return <div>Form to change password</div>;
+        return <div>Form to update Password</div>;
       case 'accountInfo':
         return <div>Form to update account information</div>;
       case '2fa':
@@ -58,7 +103,7 @@ export default function SettingsPage({ toggle }) {
   };
 
   return (
-    <main className="flex min-h-screen flex-col items-center bg-opacity-0">
+    <main className="flex h-screen w-screen flex-col items-center justify-center bg-opacity-0">
       <Card className="bg-primary border-accent mt-8">
       <CardHeader className="relative flex items-center justify-center w-full p-4">
         {activeSetting ? (
@@ -83,7 +128,7 @@ export default function SettingsPage({ toggle }) {
         
         <div className="w-4/5 mx-auto border-t border-grey-700 m-5"></div>
 
-        <CardContent className="flex flex-col items-center w-full px-20 py-10 min-h-[400px] min-w-[994px]">
+        <CardContent className="flex flex-col items-center w-full px-20 py-6">
           {!activeSetting && (
             <div className="grid grid-cols-3 gap-16">
 
@@ -140,7 +185,7 @@ export default function SettingsPage({ toggle }) {
         {/* Log out / Submit Button */}
         <CardFooter className="flex flex-col items-center space-y-4 mb-10">
           {activeSetting ? (
-            <Button variant='secondary' className="h-12 text-text text-xl w-full w-full max-w-sm mt-10 border-transparent hover:border hover:border-accent" type="submit">
+            <Button variant='secondary' className="h-12 text-text text-xl w-full max-w-sm mt-10 border-transparent hover:border hover:border-accent" type="submit">
               Submit
             </Button>
 
