8f7ef90d1887e0a280233a0beba269bfb259af22
Wilsonator123 <106590637+Wilsonator123@users.noreply.github.com>
2024-05-11 16:25:31 +0100

    Added roles to all functions, altered comment functions

--- server/middleware.js
+++ server/middleware.js
@@ -1,42 +1,34 @@
-const { validateCookie } = require('./utils/cookie.js');
-const { readJWT } = require('./utils/auth.js');
-const db = require('./database/index.js');
+const { validateCookie } = require("./utils/cookie.js");
+const { readJWT } = require("./utils/auth.js");
+const db = require("./database/index.js");
 
-const authorize = (requiredPermission=[], cookie=false, userID=null) => {
-    return async (req, res, next) => {
-        if (requiredPermission.length === 0) {
-            return next();
-        }
+const authorize = (requiredPermission = []) => {
+	return async (req, res, next) => {
+		const validCookie = await validateCookie(req, res);
 
-        if (!cookie && !userID) {
-            return res.status(403).send('Forbidden');
-        }
+		if (requiredPermission.length === 0) {
+			return next();
+		}
 
-        let permissions = [];
+		let permissions = [];
 
-        if (cookie) {
-            const validCookie = await validateCookie(req, res);
+		if (validCookie) {
+			const payload = await readJWT(req.cookies.id);
+			const id = payload.sub;
+			if (!id) {
+				return res.status(403).send("Forbidden");
+			}
+			permissions = payload.scopes;
+		} else {
+			return res.status(403).send("Forbidden");
+		}
 
-            if (validCookie) {
-                const payload = await readJWT(req.cookies.id);
-                const id = payload.sub
-                if (!id) {
-                    return res.status(403).send('Forbidden');
-                }
-                permissions = payload.scopes;
-            } else {
-                return res.status(403).send('Forbidden');
-            }
-        } else {
-            permissions = db.query('getPermissions', [userID]);
-        }
-
-        if (permissions.every(scope => requiredPermission.includes(scope))) {
-            return next();
-        } else {
-            return res.status(403).send('Forbidden');
-        }
-    }
-}
+		if (requiredPermission.every((scope) => permissions.includes(scope))) {
+			return next();
+		} else {
+			return res.status(403).send("Forbidden");
+		}
+	};
+};
 
 module.exports = { authorize };
--- server/posts/posts.js
+++ server/posts/posts.js
@@ -1,74 +1,32 @@
-const Database = require('../database/mongo.js');
-const { ObjectId } = require('mongodb');
+const Database = require("../database/mongo.js");
+const { ObjectId } = require("mongodb");
 const db = new Database();
 
 async function createPost(data) {
-    data = {
-        title: data['title'],
-        content: data['content'],
-        created_at: new Date(),
-        comments: data['comments'] ?? []
-    }
-    return await db.run(db.create_file, 'posts',  data);
+	data = {
+		title: data["title"],
+		content: data["content"],
+		created_at: new Date(),
+		comments: data["comments"] ?? [],
+	};
+	return await db.run(db.create_file, "posts", data);
 }
 
-async function getPosts(args){
-    const result = await db.run(db.read_file, 'posts', args)
-    return result
+async function getPosts(args) {
+	const result = await db.run(db.read_file, "posts", args);
+	return result;
 }
 
-async function modifyPost(args, data){
-    return await db.run(db.write_to_file, 'posts', args, {'$set': data})
+async function modifyPost(args, data) {
+	return await db.run(db.write_to_file, "posts", args, { $set: data });
 }
 
-async function deletePost(data){
-    return await db.run(db.delete_file, 'posts',  data)
-}
-
-async function modifyComment(action, args, comment = {}, data={}){
-    let result;
-    switch(action){
-        case "get":
-            result = await db.run(db.read_file, 'posts', args)
-            if(result){
-                return result.comments.filter((data) => data.message === comment['comment.message'])
-            }
-            return "Comment not found"
-
-        case "add":
-            data['comment_id'] = new ObjectId()
-            data['created_at'] = new Date()
-
-            result = await db.run(db.write_to_file, 'posts', args, {'$push': {"comments": data}})
-
-            if (result.modifiedCount > 0 || result.matchedCount > 0) {
-                return 'Comment added'
-            }
-
-            return 'Comment not added'
-
-        case "edit":
-            data['edited'] = true
-            data['edited_at'] = new Date()
-            result = await db.run(db.write_to_file, 'posts', args, {'$set': {"comments.$[comment]": data}}, { "arrayFilters": [comment] })
-            if (result.modifiedCount > 0 || result.matchedCount > 0) {
-                return 'Comment edited'
-            }
-
-            return 'Comment not found/edited'
-
-        case "delete":
-            result = await db.run(db.write_to_file, 'posts', args, {'$pull': {"comments": data}})
-            if (result.modifiedCount > 0 || result.matchedCount > 0) {
-                return 'Comment deleted'
-            }
-            return 'Comment not found/deleted'
-    }
+async function deletePost(data) {
+	return await db.run(db.delete_file, "posts", data);
 }
 
 module.exports = {
-    createPost,
-    getPosts,
-    deletePost,
-    modifyComment
+	createPost,
+	getPosts,
+	deletePost,
 };
