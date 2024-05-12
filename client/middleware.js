d11d0b051466339c93729d53d9ddd954dc144c59
Alex Taft <126822918+thealextaft@users.noreply.github.com>
2024-05-12 18:49:16 +0100

    Fix bug

--- client/middleware.js
+++ client/middleware.js
@@ -3,42 +3,52 @@ import { NextResponse } from "next/server";
 import { cookies } from "next/headers";
 const API_URL = "http://localhost:8000/";
 
-async function checkPermission(scope, cookie) {
-	return await fetch(API_URL + "auth/hasPermission", {
-		method: "POST",
-		credentials: "include",
-		headers: {
-			"Content-Type": "application/json",
-			Cookie: "id=" + cookie,
-		},
-		body: JSON.stringify({ scope }),
-	});
+async function checkPermission(scope, cookie){
+    try {
+        return await fetch(API_URL + 'auth/hasPermission', {
+            method: 'POST',
+            credentials: 'include',
+            headers: {
+                'Content-Type': 'application/json',
+                'Cookie': 'id=' + cookie
+            },
+            body: JSON.stringify({scope})
+        })
+    } catch (error) {
+        console.error(error)
+        return true
+    }
 }
 
 export async function middleware(request) {
-	const id = request.cookies.get("id")?.value ?? "";
+    const id = request.cookies.get('id')?.value || null
 
-	if (request.nextUrl.pathname.startsWith("/login")) {
-		const response = NextResponse.next();
-		if (id) {
-			const result = await fetch(API_URL + "auth/validateID", {
-				method: "GET",
-				headers: {
-					"Content-Type": "application/json",
-					Cookie: "id=" + id,
-				},
-				credentials: "include",
-			});
-			if (result.ok) {
-				return NextResponse.redirect(new URL("/", request.url));
-			} else {
-				response.cookies.set("id", "", { expires: new Date(0) });
-				return response;
-			}
-		} else {
-			return NextResponse.next();
-		}
-	}
+    if(request.nextUrl.pathname.startsWith('/login')) {
+        const response = NextResponse.next()
+        if (id) {
+            try{
+                const result = await fetch(API_URL + 'auth/validateID', {
+                    method: 'GET',
+                    headers: {
+                        'Content-Type': 'application/json',
+                        'Cookie': 'id=' + id
+                    },
+                    credentials: 'include'
+                })
+                if (result.ok) {
+                    return NextResponse.redirect(new URL('/feed', request.url))
+                } else {
+                    response.cookies.set('id', '', {expires: new Date(0)})
+                    return response
+                }
+            } catch (error) {
+                console.error(error)
+            }
+        } else {
+            return NextResponse.next()
+        }
+    }
+	
 	if (request.nextUrl.pathname.startsWith("/create")) {
 		const res = await checkPermission(["posts:create"], id);
 		if (res.ok) {
