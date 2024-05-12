0b91806cabc434f4b8c39e6d5fcae40679b1a3cf
Wilsonator123 <106590637+Wilsonator123@users.noreply.github.com>
2024-05-12 11:29:19 +0100

    Fixed Middleware

--- client/components/ui/login-form.jsx
+++ client/components/ui/login-form.jsx
@@ -21,9 +21,9 @@ import PasswordIcon from '@/assets/password.svg'
 
 
 
-export default function LoginForm({toggle}) {
-    const passwordBox = useRef();
-    const emailBox = useRef();
+export default function LoginForm({ toggle }) {
+  const passwordBox = useRef();
+  const emailBox = useRef();
   //form data states
 
   const [showPassword, setShowPassword] = useState("password");
@@ -34,104 +34,104 @@ export default function LoginForm({toggle}) {
 
 
 
-  async function handleSubmit(event){
+  async function handleSubmit(event) {
     event.preventDefault();
 
     try {
-      
-      const response = await axios.post('http://localhost:8000/login/loginChecker', {
+
+      const response = await axios.post('http://localhost:8000/login/login', {
         email: email,
         password: password
       },
-          {
-            withCredentials: true,
-                headers: {
-                'Content-Type': 'application/json',
-            }
+        {
+          withCredentials: true,
+          headers: {
+            'Content-Type': 'application/json',
+          }
         })
 
-      if(response.status === 200){
+      if (response.status === 200) {
         router.push('/home');
       }
-      
+
     } catch (error) {
-        if (error.status === 401){
-            setError("Incorrect username or password");
-            emailBox.current.focus();
-        }
-        else{
-            setError("There was a problem with the server.")
-            emailBox.current.focus();
-        }
+      if (error.status === 401) {
+        setError("Incorrect username or password");
+        emailBox.current.focus();
+      }
+      else {
+        setError("There was a problem with the server.")
+        emailBox.current.focus();
+      }
     }
 
-    
-    
+
+
   }
 
-  function forgottenPassword (){
+  function forgottenPassword() {
     router.push('/forgottenPassword');
   }
 
   return (
-      <Card className="h-3/5 w-1/2 bg-primary border-accent mt-8 rounded-3xl">
-        <CardHeader >
-          <CardTitle className="text-3xl m-auto mt-16 text-text">Sign In</CardTitle>
-        </CardHeader>
-        <CardContent className="flex justify-center w-full">
-          <form onSubmit={handleSubmit} className="relative flex flex-col justify-center w-full items-center">
-            <div className="relative flex flex-col w-3/5 justify-center items-center pb-6">
-              <p className='text-text absolute -top-2 left-1'>E-mail</p>
-              <Input id="login-email" type="email" className="my-4 h-14 bg-black border-secondary text-text
+    <Card className="h-3/5 w-1/2 bg-primary border-accent mt-8 rounded-3xl">
+      <CardHeader >
+        <CardTitle className="text-3xl m-auto mt-16 text-text">Sign In</CardTitle>
+      </CardHeader>
+      <CardContent className="flex justify-center w-full">
+        <form onSubmit={handleSubmit} className="relative flex flex-col justify-center w-full items-center">
+          <div className="relative flex flex-col w-3/5 justify-center items-center pb-6">
+            <p className='text-text absolute -top-2 left-1'>E-mail</p>
+            <Input id="login-email" type="email" className="my-4 h-14 bg-black border-secondary text-text
               pl-12 focus:border-accent"
               required value={email} autoComplete="email"
               onChange={(e) => setEmail(e.target.value)}
               ref={emailBox} />
-              <div className="text-text absolute left-2 z-10">
-                <Mail width={30} height={30} fill={'#fff'}/>
-              </div>
+            <div className="text-text absolute left-2 z-10">
+              <Mail width={30} height={30} fill={'#fff'} />
             </div>
-            <div className="relative flex flex-col w-3/5 justify-center items-center">
-                <p className='text-text absolute -top-2 left-1'>Password</p>
-                <Input id="login-password" type={`${showPassword}`} className="my-4 h-14 bg-black border-secondary text-text
+          </div>
+          <div className="relative flex flex-col w-3/5 justify-center items-center">
+            <p className='text-text absolute -top-2 left-1'>Password</p>
+            <Input id="login-password" type={`${showPassword}`} className="my-4 h-14 bg-black border-secondary text-text
                 pl-12 focus:border-accent"
-                required value={password} autoComplete="current-password"
-                onChange={(e) => setPassword(e.target.value)}
-                ref={passwordBox}
-                />
-                <div className="text-text absolute left-2 z-10">
-                  <PasswordIcon  fill={'#fff'}/>
-                </div> 
-              <div onClick={() => {
-                  setShowPassword(prev => prev === 'text' ? 'password' : 'text')
-                  passwordBox.current.focus();
+              required value={password} autoComplete="current-password"
+              onChange={(e) => setPassword(e.target.value)}
+              ref={passwordBox}
+            />
+            <div className="text-text absolute left-2 z-10">
+              <PasswordIcon fill={'#fff'} />
+            </div>
+            <div onClick={() => {
+              setShowPassword(prev => prev === 'text' ? 'password' : 'text')
+              passwordBox.current.focus();
 
-              }} className="text-text absolute right-2 z-10">
-                  {showPassword === 'password' ? <HidePassword width={30} height={30} fill={'#fff'}/> : <ShowPassword width={30} height={30} fill={'#fff'}/>}
-              </div>
+            }} className="text-text absolute right-2 z-10">
+              {showPassword === 'password' ? <HidePassword width={30} height={30} fill={'#fff'} /> : <ShowPassword width={30} height={30} fill={'#fff'} />}
+            </div>
           </div>
           <div className='flex flex-row w-3/5 -mt-3 mb-8 justify-end'>
             <a onClick={forgottenPassword} className='text-text 
              underline self-end'>Forgot your password?</a>
           </div>
           <div className="flex justify-center items-center">
-              <Button variant='secondary' className="h-12 bg-secondary text-text text-xl w-96 max-w-xs
+            <Button variant='secondary' className="h-12 bg-secondary text-text text-xl w-96 max-w-xs
                hover:border hover:border-accent" type="submit" onSubmit={handleSubmit}>Sign In</Button>
           </div>
-            {error && <Alert className="mt-4 text-white"><AlertDescription id="error-result">{error}</AlertDescription></Alert>}
-          </form>
-        </CardContent>
-        <div className='flex flex-row items-center'>
-          <hr className='border-text w-96 ml-16'/><p className='text-text m-4'>Or</p><hr className='border-text w-96'/>
+          {error && <Alert className="mt-4 text-white"><AlertDescription id="error-result">{error}</AlertDescription></Alert>}
+        </form>
+      </CardContent>
+      <div className='flex flex-row items-center'>
+        <hr className='border-text w-96 ml-16' /><p className='text-text m-4'>Or</p><hr className='border-text w-96' />
+      </div>
+      <CardFooter className="flex flex-col items-center space-y-4 mb-32">
+        <Oauth />
+        <div>
+          <p onClick={toggle} className='text-text'>Don't have an account? <a onClick={toggle}
+            className='text-text underline hover:border-accent'>Signup.</a> </p>
         </div>
-        <CardFooter className="flex flex-col items-center space-y-4 mb-32">
-            <Oauth />
-            <div>
-              <p onClick={toggle} className='text-text'>Don't have an account? <a onClick={toggle}
-               className='text-text underline hover:border-accent'>Signup.</a> </p>
-            </div>
-        </CardFooter>
-        
-      </Card>
+      </CardFooter>
+
+    </Card>
   );
 }
--- client/middleware.js
+++ client/middleware.js
@@ -1,76 +1,73 @@
-'use server'
-import {NextResponse} from 'next/server'
-import { cookies } from 'next/headers'
-const API_URL = 'http://localhost:8000/'
+"use server";
+import { NextResponse } from "next/server";
+import { cookies } from "next/headers";
+const API_URL = "http://localhost:8000/";
 
 // This function can be marked `async` if using `await` inside
 
 // I think we should give ids to unauthenticated users which allow them to read
 
 // There should be routes for users /account, /create etc.
-async function checkPermission(scope, cookie){
-    return await fetch(API_URL + 'auth/hasPermission', {
-        method: 'POST',
-        credentials: 'include',
-        headers: {
-            'Content-Type': 'application/json',
-            'Cookie': 'id=' + cookie
-        },
-        body: JSON.stringify({scope})
-    })
+async function checkPermission(scope, cookie) {
+	return await fetch(API_URL + "auth/hasPermission", {
+		method: "POST",
+		credentials: "include",
+		headers: {
+			"Content-Type": "application/json",
+			Cookie: "id=" + cookie,
+		},
+		body: JSON.stringify({ scope }),
+	});
 }
 // There should be admin routes /admin
 export async function middleware(request) {
-    const id = request.cookies.get('id')?.value ?? ''
+	const id = request.cookies.get("id")?.value ?? "";
 
-    if(request.nextUrl.pathname.startsWith('/login')){
-        const response =  NextResponse.next()
-        if(id){
-            const result = await fetch(API_URL + 'auth/validateID', {
-                method: 'GET',
-                headers: {
-                    'Content-Type': 'application/json',
-                    'Cookie': 'id=' + id
-                },
-                credentials: 'include'
-            })
-            if(result.ok){
-                return NextResponse.redirect(new URL('/feed', request.url))
-            }else{
-                response.cookies.set('id', '', {expires: new Date(0)})
-                return response
-            }
-        }
-        else{
-            return NextResponse.next()
-        }
-    }
-    if(request.nextUrl.pathname.startsWith('/create')){
-        const res = await checkPermission(['user:write'], id)
-        if(res.ok){
-            return NextResponse.next()
-        } else {
-            return NextResponse.redirect(new URL('/login', request.url))
-        }
-    }
-    if(request.nextUrl.pathname.startsWith('/account')){
-        const res = await checkPermission(['user:logged_in', 'user:read'], id)
-        if(res.ok){
-            return NextResponse.next()
-        } else {
-            return NextResponse.redirect(new URL('/login', request.url))
-        }
-    }
-    if(request.nextUrl.pathname.startsWith('/admin')){
-        const res = await checkPermission(['admin'], id)
-        if(res.ok){
-            return NextResponse.next()
-        } else {
-            return NextResponse.redirect(new URL('/404', request.url))
-        }
-    }
+	if (request.nextUrl.pathname.startsWith("/login")) {
+		const response = NextResponse.next();
+		if (id) {
+			const result = await fetch(API_URL + "auth/validateID", {
+				method: "GET",
+				headers: {
+					"Content-Type": "application/json",
+					Cookie: "id=" + id,
+				},
+				credentials: "include",
+			});
+			if (result.ok) {
+				return NextResponse.redirect(new URL("/", request.url));
+			} else {
+				response.cookies.set("id", "", { expires: new Date(0) });
+				return response;
+			}
+		} else {
+			return NextResponse.next();
+		}
+	}
+	if (request.nextUrl.pathname.startsWith("/create")) {
+		const res = await checkPermission(["posts:create"], id);
+		if (res.ok) {
+			return NextResponse.next();
+		} else {
+			return NextResponse.redirect(new URL("/login", request.url));
+		}
+	}
+	if (request.nextUrl.pathname.startsWith("/account")) {
+		const res = await checkPermission(["account:read"], id);
+		if (res.ok) {
+			return NextResponse.next();
+		} else {
+			return NextResponse.redirect(new URL("/login", request.url));
+		}
+	}
+	if (request.nextUrl.pathname.startsWith("/admin")) {
+		const res = await checkPermission(["admin"], id);
+		if (res.ok) {
+			return NextResponse.next();
+		} else {
+			return NextResponse.redirect(new URL("/404", request.url));
+		}
+	}
 }
 
-export const config = {
-
-}
+export const config = {};
