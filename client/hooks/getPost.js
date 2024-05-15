34168638652802a1eff6c20abe3c611d09c5b802
Wilsonator123 <106590637+Wilsonator123@users.noreply.github.com>
2024-05-15 14:04:37 +0100

    tryign to get a good branch

--- client/hooks/getPost.js
+++ client/hooks/getPost.js
@@ -1,13 +1,17 @@
-'use server'
-import axios from 'axios';
+"use server";
+import axios from "axios";
 
 export const getPost = async (postId) => {
-    'use server'
-    try {
-        const response = await axios.post('http://localhost:8000/posts/getPosts', {args: {_id: postId}})
-        return response.data.data
-    } catch (error) {
-        console.error(error)
-        return []
-    }
-}
+	"use server";
+	try {
+		const response = await axios.post(
+			"http://localhost:8000/posts/getPosts",
+			{ args: { _id: postId } }
+		);
+
+		return response.data.data;
+	} catch (error) {
+		console.error(error);
+		return [];
+	}
+};
--- client/hooks/usePosts.js
+++ client/hooks/usePosts.js
@@ -1,8 +1,10 @@
 "use server";
 import axios from "axios";
+import { cookies } from "next/headers";
 
 export const usePosts = async (args = {}) => {
 	"use server";
+	const id = request.cookies.get("id")?.value ?? "";
 	try {
 		const response = await axios.post(
 			"http://localhost:8000/posts/getPosts",
@@ -12,6 +14,7 @@ export const usePosts = async (args = {}) => {
 				headers: {
 					"Content-Type": "application/json",
 				},
+				Cookies: "id=" + id,
 			}
 		);
 		return response.data.data;
